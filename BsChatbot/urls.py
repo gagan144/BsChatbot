1e60402dea405400a202b3eeb2e15a38d3bc5dc9
Gagandeep <singh.gagan144@gmail.com>
2020-04-02 01:38:47 +0530

    Heroku deployment - Minor change in urls.py

--- BsChatbot/urls.py
+++ BsChatbot/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 from core import views as views_core
 
@@ -27,4 +28,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('chat/', include('core.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
