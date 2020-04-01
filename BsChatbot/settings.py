04211e4407220d6e3d477d72983f0139101f3dd8
Gagandeep <singh.gagan144@gmail.com>
2020-04-02 01:01:59 +0530

    Minor corrections

--- BsChatbot/settings.py
+++ BsChatbot/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bschatbot',
         'USER': 'postgres',
-        'PASSWORD': 'pass',
+        'PASSWORD': '!root1234@',
         'HOST': '127.0.0.1',
         'PORT': 5432
     }
